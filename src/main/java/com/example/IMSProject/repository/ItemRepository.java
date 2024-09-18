e49cd5205db2570a7eb7425df1c8d61b3a1f13cc
Sasi <sede@ttu.edu>
2024-09-17 23:49:24 -0500

    Updated the ItemRepository Class

--- src/main/java/com/example/IMSProject/repository/ItemRepository.java
+++ src/main/java/com/example/IMSProject/repository/ItemRepository.java
@@ -3,5 +3,5 @@ package com.example.IMSProject.repository;
 import com.example.IMSProject.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ItemRepository extends JpaRepository<Item,Long> {
+public interface ItemRepository extends JpaRepository<Item,Integer> {
 }
